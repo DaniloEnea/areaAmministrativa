607755625662cc8d1cd3e14f59e924acbdeb77be
DaniloEnea <danilo38.enea@gmail.com>
2023-11-16 16:21:59 +0100

    added Angular Material library

--- area_Amministrativa/src/app/app.module.ts
+++ area_Amministrativa/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrganizzazioneComponent } from './organizzazione/organizzazione.component';
 import { PersoneComponent } from './persone/persone.component';
 import { UtentiComponent } from './utenti/utenti.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,11 @@ import { UtentiComponent } from './utenti/utenti.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'organizzazione', component : OrganizzazioneComponent}
+    ]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
