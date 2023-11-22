95f47ee821b7b73e3ca6f4fa1a44334d9a9aea48
Patrick <94932581+jtabilas@users.noreply.github.com>
2023-11-22 09:38:17 +0100

    Revert "Front end"

--- area_Amministrativa/src/app/utenti/utenti.component.ts
+++ area_Amministrativa/src/app/utenti/utenti.component.ts
@@ -1,18 +1,4 @@
 import { Component } from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {ModaleAddComponent} from "./modale-add/modale-add.component";
-
-export interface UserDTO{
-  username: string;
-  position: number;
-  password: string;
-  role: string;
-}
-
-const ELEMENT_DATA: UserDTO[] = [
-  {position: 1, username: 'Simonsa', password: 'asas@as', role: 'ROLE_ADMIN'},
-  {position: 2, username: 'Lucas', password: '121das', role: 'ROLE_USER'},
-];
 
 @Component({
   selector: 'app-utenti',
@@ -20,22 +6,5 @@ const ELEMENT_DATA: UserDTO[] = [
   styleUrls: ['./utenti.component.css']
 })
 export class UtentiComponent {
-  displayedColumns: string[] = ['position', 'username', 'password', 'role', 'update'];
-  dataSource = ELEMENT_DATA;
-  protected readonly alert = alert;
-
-   // modal
- constructor(private dialog: MatDialog) {}
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ModaleAddComponent,{
-      width:'60%',   // Set width to 60%  of the window's total width
-      height:'50%',  // Set height to 50% of the window's total height
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
 
 }
