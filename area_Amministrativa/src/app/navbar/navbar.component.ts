3882d5e03c159cc0a2362f3eecfba444b12cba77
DaniloEnea <danilo38.enea@gmail.com>
2023-12-27 17:43:26 +0100

    hide navbar login area

--- area_Amministrativa/src/app/navbar/navbar.component.ts
+++ area_Amministrativa/src/app/navbar/navbar.component.ts
@@ -1,27 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { SidenavService } from '../service/SidenavService';
 import {Router} from "@angular/router";
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  constructor(private sidenavService: SidenavService, private router: Router) {
+  isLoggedIn$!: Observable<boolean>;
+
+  constructor(private sidenavService: SidenavService, private router: Router, private authService: AuthService) {
 
   }
+    ngOnInit(): void {
+      this.isLoggedIn$ = this.authService.isLoggedIn;
+    }
 
   toggleSidenav() {
     this.sidenavService.toggle();
   }
 
   logout() {
-    localStorage.removeItem("accessToken");
-    this.router.navigate([('')]).then(r => null);
-    console.log("logout successful");
+    this.authService.logout();
   }
 
 }
--- area_Amministrativa/src/app/service/auth.service.ts
+++ area_Amministrativa/src/app/service/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import {JwtHelperService} from "@auth0/angular-jwt";
+import { BehaviorSubject } from 'rxjs';
 
 
 @Injectable({
@@ -7,14 +9,29 @@ import {JwtHelperService} from "@auth0/angular-jwt";
 })
 export class AuthService {
 
-  constructor() { }
+  private loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  get isLoggedIn() {
+    return this.loggedIn.asObservable();
+  }
+
+  constructor(private router: Router) { }
 
     jwtHelper = new JwtHelperService();
 
     public isAuthenticated(): boolean {
-    const token = localStorage.getItem('accessToken');
-    // Check whether the token is expired and return
-    // true or false
-    return !this.jwtHelper.isTokenExpired(token);
+      const token = localStorage.getItem('accessToken');
+      // Check whether the token is expired and return
+      // true or false
+      this.loggedIn.next(true);
+      return !this.jwtHelper.isTokenExpired(token);
+    }
+
+  logout() {
+    localStorage.removeItem("accessToken");
+    this.loggedIn.next(false);
+    this.router.navigate([('')]);
+    console.log("logout successful");
   }
+    
 }
